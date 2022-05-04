9b921a76330334dfc713e4cbf958bfa8f5b9a7f2
Anthony Li <anthony20612000@gmail.com>
2022-05-03 21:14:03 -0400

    fixed up the routing

---
+++ src/components/ListeningRoom.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+import useAuth from "./useAuth"
+import Player from "./Player"
+import TrackSearchResult from "./TrackSearchResult"
+import { Container, Form } from "react-bootstrap"
+import SpotifyWebApi from "spotify-web-api-node"
+import axios from "axios"
+import requests from "requests"
+import { Button } from "bootstrap"
+import Alert from 'sweetalert2';
+import { Redirect, Link, useHistory } from 'react-router-dom';
+
+
+export default function ListeningRoom({accessToken, playingTrack}) {
+
+    return (
+        <div style={{'textAlign': 'center'}}> 
+            <p> Listening!!!</p>
+            <div>
+                <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+            </div>
+        </div>
+    )  
+}
