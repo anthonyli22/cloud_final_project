360f4964f506da3331294389fb871413e562a121
Anthony Li <anthony20612000@gmail.com>
2022-05-12 13:23:21 -0400

    removed warning

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -39,7 +39,7 @@ export default function Dashboard({ changeLeader, accessToken, setGroupID, setMy
 
   useEffect(() => {
     getID()
-  }, [accessToken])
+  })
 
   const createGroup = (e) => {
     e.preventDefault()
