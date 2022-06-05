985e6da893f21220d7cd9f3d8cda24d68d3cff92
Anthony Li <anthony20612000@gmail.com>
2022-06-05 11:53:44 -0700

    removed firebase stuff

--- src/components/urls.js
+++ src/components/urls.js
@@ -1,5 +1,5 @@
-// const backendURL = "https://cloudback.azurewebsites.net/"
-const backendURL = "http://localhost:5000/"
+const backendURL = "https://cloudback.azurewebsites.net/"
+// const backendURL = "http://localhost:5000/"
 const spotifyURL = 'https://api.spotify.com/v1/'
 const frontendURL = "http://localhost:8080/"
 
